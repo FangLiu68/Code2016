48dd98a5b6d4365239950849b79f209daa902459
Fang Liu <lfang68@gmail.com>
2016-07-07 11:56:20 -0700

    [169. Majority Element]committed by Fang Liu

---
+++ 169-Majority-Element/solution.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        if(nums.empty()){
+            return INT_MIN;
+        }
+        
+        int res = nums[0];
+        int count = 1;
+        
+        for(int i=1; i<nums.size(); ++i){
+            if(nums[i] == res){
+                count++;
+            }else{
+                if(count == 0){
+                    res = nums[i];
+                    count = 1;
+                }else{
+                    count--;
+                }
+            }
+        }
+        
+        return res;
+    }
+};
