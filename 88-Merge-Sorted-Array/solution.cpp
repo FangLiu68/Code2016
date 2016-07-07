1079003de0011b308b4fa6bfe95501e31b67a841
Fang Liu <lfang68@gmail.com>
2016-07-06 17:57:04 -0700

    [88. Merge Sorted Array]committed by Fang Liu

--- 88-Merge-Sorted-Array/solution.cpp
+++ 88-Merge-Sorted-Array/solution.cpp
@@ -2,19 +2,19 @@ class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
         if(nums1.empty() || nums2.empty() || m<0 || n<0){
-        return;
-    }
+            return;
+        }
     
-    int last_merge_index = m+n-1;
-    int last1 = m-1;
-    int last2 = n-1;
+        int last_merge_index = m+n-1;
+        int last1 = m-1;
+        int last2 = n-1;
     
-    while(last1>=0 && last2>=0){
-        nums1[last_merge_index--] = (nums1[last1]>=nums2[last2])? nums1[last1--] : nums2[last2--];
-    }
+        while(last1>=0 && last2>=0){
+            nums1[last_merge_index--] = (nums1[last1]>=nums2[last2])? nums1[last1--] : nums2[last2--];
+        }
     
-    while(last2 >= 0){
-        nums1[last_merge_index--] = nums2[last2--];
-    }
+        while(last2 >= 0){
+            nums1[last_merge_index--] = nums2[last2--];
+        }
     }
 };
