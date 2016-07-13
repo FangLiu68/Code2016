c0102b768e9b41191c71c06240a262f14f702c04
Fang Liu <lfang68@gmail.com>
2016-07-13 14:40:59 -0700

    [118. Pascal's Triangle]committed by Fang Liu

--- 118-Pascal's-Triangle/solution.cpp
+++ 118-Pascal's-Triangle/solution.cpp
@@ -2,19 +2,20 @@ class Solution {
 public:
     vector<vector<int>> generate(int numRows) {
         vector<vector<int>> res;
-        if(numRows <= 0) return res;
+        if(numRows <= 0){
+            return res;
+        }
         
-        res.push_back(vector<int>(1,1)); // push first row into res
+        res.push_back(vector<int>(1,1));
         
         for(int i=2; i<=numRows; ++i){
-            vector<int> cur(i, 1); // initialize i elements in this row, and all has value 1
-            vector<int> pre = res[i-2]; // previous row
+            vector<int> cur(i, 1);
+            vector<int> pre = res[i-2];
             for(int j=1; j<i-1; ++j){
-                cur[j] = pre[j-1] + pre[j];
+                cur[j] = pre[j-1] + pre[j];   
             }
             res.push_back(cur);
         }
-        
         return res;
     }
 };
