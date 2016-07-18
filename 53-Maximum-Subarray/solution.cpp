12e617fae7ffd9d26a0a87df8e09a073743c72b6
Fang Liu <lfang68@gmail.com>
2016-07-17 21:20:10 -0700

    [53. Maximum Subarray]committed by Fang Liu

--- 53-Maximum-Subarray/solution.cpp
+++ 53-Maximum-Subarray/solution.cpp
@@ -5,13 +5,15 @@ public:
             return INT_MIN;
         }
         
-        vector<int> record(nums.size(), 0);
-        record[0] = nums[0];
-        int res = record[0];
+        //vector<int> record(nums.size(), 0);
+        //record[0] = nums[0];
+        int cur_max = nums[0];
+        int res = cur_max;
         
         for(int i=1; i<nums.size(); ++i){
-            record[i] = max(record[i-1]+nums[i], nums[i]);
-            res = max(res, record[i]);
+            //record[i] = max(record[i-1]+nums[i], nums[i]);
+            cur_max = max(cur_max + nums[i], nums[i]);
+            res = max(res, cur_max);
         }
         
         return res;
