526e57587d5eaedf1d2ff1e3fc36798992989384
Fang Liu <lfang68@gmail.com>
2016-07-27 12:51:15 -0700

    [63. Unique Paths II]committed by Fang Liu

--- 63-Unique-Paths-II/solution.cpp
+++ 63-Unique-Paths-II/solution.cpp
@@ -6,16 +6,20 @@ public:
         if(row <= 0 || col <= 0) return 0;
         if(obstacleGrid[0][0] == 1) return 0;
         
-        vector<int> steps(col, 0);
-        steps[0] = 1;
+        vector<int> steps(col, 1);
         
-        for(int i=0; i<row; ++i){
-            for(int j=0; j<col; ++j){
-                if(obstacleGrid[i][j] == 0 && j > 0){
-                    steps[j] = steps[j-1] + steps[j];
-                }else if(obstacleGrid[i][j] == 1){
-                    steps[j] = 0;
-                }
+        for(int j = 1; j < col; ++j){
+            if(obstacleGrid[0][j] == 1){
+                steps[j] = 0;
+            }else{
+                steps[j] = steps[j-1];
+            }
+        }
+        
+        for(int i = 1; i < row; ++i){
+            steps[0] = obstacleGrid[i][0] == 1? 0 : steps[0];
+            for(int j = 1; j < col; ++j){
+                steps[j] = obstacleGrid[i][j] == 1? 0 : steps[j]+steps[j-1];
             }
         }
         
