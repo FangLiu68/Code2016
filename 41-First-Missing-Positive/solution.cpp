77ec63622aaf4d4a135ac588448fdf600000b0cf
Fang Liu <lfang68@gmail.com>
2016-07-27 13:44:16 -0700

    [41. First Missing Positive]committed by Fang Liu

---
+++ 41-First-Missing-Positive/solution.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int firstMissingPositive(vector<int>& nums) {
+        if(nums.empty()){
+            return 1;
+        }
+        
+        int i = 0;
+        while(i < nums.size()){
+            if(nums[i]>0 && nums[i]<=nums.size() && nums[i]!=i+1 && nums[i]!=nums[nums[i]-1]){
+                swap(nums[i], nums[nums[i]-1]);
+            }else{
+                i++;
+            }
+        }
+        
+        for(int i=0; i<nums.size(); ++i){
+            if(nums[i] != i+1){
+                return i+1;
+            }
+        }
+        
+        return nums.size()+1;
+    }
+};
