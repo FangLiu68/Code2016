36365abc0968716ea94e3ee2be21a176205be114
Fang Liu <lfang68@gmail.com>
2016-08-10 13:49:54 -0700

    [209. Minimum Size Subarray Sum]committed by Fang Liu

---
+++ 209-Minimum-Size-Subarray-Sum/solution.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int minSubArrayLen(int s, vector<int>& nums) {
+        if(nums.empty()){
+            return 0;
+        }
+        
+        int left = 0;
+        int right = 0;
+        int res = INT_MAX;
+        int cur_sum = 0;
+        
+        while(right < nums.size()){
+            cur_sum = cur_sum + nums[right];
+            while(cur_sum >= s){
+                res = min(res, right-left+1);
+                cur_sum = cur_sum - nums[left];
+                left++;
+            }
+            right++;
+        }
+        
+        return res!=INT_MAX? res:0;
+    }
+};
