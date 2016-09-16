15d5356a00ba5f9912687a71d256bd1cf38f87ec
Fang Liu <lfang68@gmail.com>
2016-09-16 15:08:10 -0700

    [234. Palindrome Linked List]committed by Fang Liu

--- 234-Palindrome-Linked-List/solution.cpp
+++ 234-Palindrome-Linked-List/solution.cpp
@@ -8,29 +8,65 @@
  */
 class Solution {
 public:
-    // a b a; a a
     bool isPalindrome(ListNode* head) {
-        if(head == NULL) return true;
-        stack<ListNode*> stk;
-        ListNode* slow = head;
-        ListNode* fast = head;
-        
-        while(fast && fast->next){
-            stk.push(slow);
-            slow = slow->next;
-            fast = fast->next->next;
-        }
-        if(fast) slow = slow->next;
-        
-        while(!stk.empty()){
-            ListNode* cur = stk.top();
-            stk.pop();
-            if(cur->val != slow->val){
-                return false;
-            }
-            slow = slow->next;
-        }
-        
+        if(head == NULL || head->next == NULL){
         return true;
     }
+    
+    ListNode* cur = head;
+    ListNode* rightHead = find_middleNode(cur);
+    ListNode* reverse_rightHead = reverse_rightHalf(rightHead);
+    ListNode* cur_right = reverse_rightHead;
+    while(cur_right){
+        if(cur->val != cur_right->val){
+            return false;
+        }
+        cur = cur->next;
+        cur_right = cur_right->next;
+    }
+    
+    return true;
+
+    }
+    
+    // find the middle node of the list, return the right half list's head node
+ListNode* find_middleNode(ListNode* head){
+    if(head == NULL || head->next == NULL){
+        return head;
+    }
+    
+    ListNode* slow = head;
+    ListNode* fast = head;
+    
+    // a b a; a a
+    while(fast && fast->next){
+        slow = slow->next;
+        fast = fast->next->next;
+    }
+    
+    if(fast){
+        slow = slow->next;
+    }
+    
+    return slow;
+}
+
+// reverse a single linked list
+ListNode* reverse_rightHalf(ListNode* head){
+    if(head == NULL || head->next == NULL){
+        return head;
+    }
+    
+    ListNode* pre = NULL;
+    ListNode* cur = head;
+    
+    while(cur){
+        ListNode* next_node = cur->next;
+        cur->next = pre;
+        pre = cur;
+        cur = next_node;
+    }
+    
+    return pre;
+}
 };
